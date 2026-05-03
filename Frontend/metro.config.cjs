06af0da8ef3b3be4ec6a4bc7ec4d17ccc2fe24ea
im23-pilzm <pilzm@bzz.ch>
2026-05-03 12:43:55 +0200

    Tiny fix with metro.config. - Helps that there are no confilcts with "document"

--- Frontend/metro.config.cjs
+++ Frontend/metro.config.cjs
@@ -3,4 +3,20 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.resolverMainFields = [
+  'react-native',
+  'native',
+  'main',
+  'module',
+];
+
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
+// Use forward-slash-safe regex patterns for Windows
+config.resolver.blockList = [
+  /expo-router.build.rsc.router.client\.js$/,
+  /expo-router.vendor.react-helmet-async.lib.index\.esm\.js$/,
+  /expo-router.vendor.react-helmet-async.lib.index\.js$/,
+];
+
 module.exports = withNativeWind(config, { input: './global.css' });
